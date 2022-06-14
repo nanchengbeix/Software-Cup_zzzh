cb36bbfb4019cdc6b1e6ff2a1a31f06d5f672e4d
huxianshen <3185222305@qq.com>
2022-06-14 11:06:23 +0800

    根据前端携带经纬度的请求返回当地城市天气

---
+++ src/main/java/com/ycu/zzzh/visual_impairment_3zh/controller/WeatherController.java
@@ -0,0 +1,24 @@
+package com.ycu.zzzh.visual_impairment_3zh.controller;
+
+import com.alibaba.fastjson.JSONObject;
+import com.ycu.zzzh.visual_impairment_3zh.utils.weather.WeatherSearcher;
+import lombok.SneakyThrows;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ClassName WeatherController
+ * @description: 根据前端携带经纬度的请求返回当地城市天气
+ * @Date 2022/6/13 20:25
+ * @Version 1.0
+ **/
+@RestController
+public class WeatherController {
+    @SneakyThrows
+    @RequestMapping("weather")
+    public JSONObject getWeather(@RequestParam() String longitude, @RequestParam String latitude){
+        return WeatherSearcher.getLocation(longitude,latitude);
+    }
+}
